adf0a7f4daef6586b4ce5eca0420671426499375
rijalmyd <muhyidinrijal@gmail.com>
2025-08-26 13:50:50 +0700

    first

---
+++ LatihanMengunduhGambar2/ImageDownloader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageDownloader.swift
+//  LatihanMengunduhGambar2
+//
+//  Created by IT Bawon 2 on 26/08/25.
+//
+
+import Foundation
---
+++ LatihanMengunduhGambar2/MovieModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieModel.swift
+//  LatihanMengunduhGambar2
+//
+//  Created by IT Bawon 2 on 26/08/25.
+//
+
+import Foundation
