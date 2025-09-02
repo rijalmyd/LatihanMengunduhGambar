c5ac597ece192792b9868818a56703efe7995a83
rijalmyd <muhyidinrijal@gmail.com>
2025-09-02 14:58:45 +0700

    update get data from network

---
+++ LatihanMengunduhGambar2/View/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  LatihanMengunduhGambar2
+//
+//  Created by IT Bawon 2 on 26/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var movieTableView: UITableView!
+    private var movies: [Movie] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        movieTableView.dataSource = self
+        // Do any additional setup after loading the view.
+        movieTableView.register(
+            UINib(nibName: "MovieTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "movieTableViewCell"
+        )
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Task { await getMovies() }
+    }
+    
+    func getMovies() async {
+        let network = NetworkService()
+        do {
+            movies = try await network.getMovies()
+            movieTableView.reloadData()
+        } catch {
+            fatalError("Error: connection failed.")
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(
+            withIdentifier: "movieTableViewCell",
+            for: indexPath
+        ) as? MovieTableViewCell {
+            
+            let movie = movies[indexPath.row]
+            cell.movieTitle.text = movie.title
+            cell.movieImage.image = movie.image
+            
+            if movie.state == .new {
+                cell.indicatorLoading.isHidden = false
+                cell.indicatorLoading.startAnimating()
+                startDownload(movie: movie, indexPath: indexPath)
+            } else {
+                cell.indicatorLoading.stopAnimating()
+                cell.indicatorLoading.isHidden = true
+            }
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    fileprivate func startDownload(movie: Movie, indexPath: IndexPath) {
+        let imageDownloader = ImageDownloader()
+        if movie.state == .new {
+            Task {
+                do {
+                    let image = try await imageDownloader.downloadImage(url: movie.posterPath)
+                    movie.state = .downloaded
+                    movie.image = image
+                    self.movieTableView.reloadRows(at: [indexPath], with: .automatic)
+                } catch {
+                    movie.state = .failed
+                    movie.image = nil
+                }
+            }
+        }
+    }
+}
